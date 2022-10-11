1a75b9f6239f10064e1674339a2e0ffee4fe2c3f
Thomas Conger <t.e.conger@gmail.com>
2022-10-10 17:29:26 -0700

    Solved number guessing game

--- guessing_game_project/lib/guessing_game.rb
+++ guessing_game_project/lib/guessing_game.rb
@@ -1,3 +1,35 @@
 class GuessingGame
+    attr_reader :num_attempts
 
+    def initialize (min, max)
+        @min = min
+        @max = max
+        @secret_num = rand(min..max)
+        @num_attempts = 0
+        @game_over = false
+    end
+
+    def game_over?
+        @game_over
+    end
+
+    def check_num(num)
+        @num_attempts += 1
+        if num == @secret_num
+            @game_over = true
+            p "You win!"
+        elsif num < @secret_num
+            #small
+            p "Oh no. Too small."
+            p "----------"
+        else
+            p "Oh no. Not that big."
+        end
+    end
+
+    def ask_user
+        p "Enter a number."
+        guess = gets.chomp.to_i
+        self.check_num(guess)
+    end
 end
