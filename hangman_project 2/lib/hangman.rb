ce2f86fbf0b0ed337f87f50fa6e7bd2bd035064d
Thomas Conger <t.e.conger@gmail.com>
2022-10-11 10:33:10 -0700

    Solved hangman

--- hangman_project 2/lib/hangman.rb
+++ hangman_project 2/lib/hangman.rb
@@ -1,3 +1,5 @@
+require "byebug"
+
 class Hangman
   DICTIONARY = ["cat", "dog", "bootcamp", "pizza"]
 
@@ -39,6 +41,58 @@ class Hangman
     end
   end
 
+  def try_guess(guess)
+    if self.already_attempted?(guess)
+      puts "that has already been attempted"
+      false
+    else
+        @attempted_chars << guess
+        #check  i f matching indices, i f so fill
+        m_indices = self.get_matching_indices(guess)
+        if m_indices == []
+          @remaining_incorrect_guesses -= 1
+        else
+          self.fill_indices(guess, m_indices)
+        end
+        true
+    end #if already attempted
+  end #try guess
 
+  def ask_user_for_guess
+    puts "Enter a char:"
+    attempt = gets.chomp
+    self.try_guess(attempt)
+  end
 
-end
+  def win?
+    user_thinks = @guess_word.join("")
+    actual = @secret_word
+    if user_thinks == actual
+      puts "WIN"
+      true
+    else
+      false
+    end
+
+  end #win
+
+  def lose?
+    if @remaining_incorrect_guesses == 0
+      puts "LOSE"
+      true
+    else
+      false
+    end
+  end #lose?
+
+  def game_over?
+
+     if self.win? || self.lose?
+      puts @secret_word
+      true
+     else
+      false
+     end
+
+  end #game_over?
+end  #hangman
