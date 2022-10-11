4f0b36f5a671b6c395e6e1af31f38c2bf84714be
Thomas Conger <t.e.conger@gmail.com>
2022-10-11 14:13:02 -0700

    completed hotel

--- hotel_project/lib/hotel.rb
+++ hotel_project/lib/hotel.rb
@@ -1,5 +1,55 @@
 require_relative "room"
 
 class Hotel
-  
+
+    def initialize(name, rooms)
+        @name = name
+        @rooms = Hash.new()
+        rooms.each do |k, v|
+            @rooms[k] = Room.new(v)
+        end
+    end
+
+    def name
+        words = @name.split(" ")
+        capitalized = words.map {|ele| ele[0].upcase + ele[1..-1]}
+        capitalized.join(" ")
+
+    end
+
+    def rooms
+        @rooms
+    end
+
+    def room_exists?(room_name)
+        @rooms.has_key?(room_name)
+    end
+
+    def check_in(person, room)
+        if self.room_exists?(room)
+            unless @rooms[room].add_occupant(person)
+                puts "sorry, room is full"
+            end
+            puts "check in successful"
+        else
+            puts "sorry, room does not exist"
+        end
+    end
+
+    def has_vacancy?
+        #if all hotel rooms are full, return false
+        #else, return true
+        no_vacancy = @rooms.all? do |name, room|
+            room.full?
+        end
+        if no_vacancy then return false else return true end
+    end
+
+    def list_rooms
+        #print every room name, with available spaces
+        #use available space
+        @rooms.each do |name, room|
+            puts name + ".*" + room.available_space.to_s
+        end
+    end
 end
--- hotel_project/lib/room.rb
+++ hotel_project/lib/room.rb
@@ -1,3 +1,36 @@
 class Room
-  
-end
+
+    attr_reader :capacity, :occupants
+
+    def initialize(capacity)
+        @capacity = capacity
+        @occupants = Array.new()
+    end
+
+    def full?
+        occupants = @occupants.length
+        capacity = @capacity
+
+        if  occupants >= capacity
+            return true
+        else
+            return false
+        end
+    end
+
+    def available_space
+        @capacity - @occupants.length
+    end
+
+    def add_occupant(name)
+        if self.full?
+            return false
+        else
+            @occupants << name
+            return true
+        end
+    end
+
+
+
+end #room
